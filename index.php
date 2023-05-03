da8cb2c7e4c243661e4886c0f1b0dc440261755e
Ghlen Nagels <ghlen@pm.me>
2023-05-03 19:53:42 +0530

    setup php cs fixer

--- index.php
+++ index.php
@@ -4,25 +4,24 @@ declare(strict_types=1);
 
 use Laudis\Neo4j\Authentication\Authenticate;
 use Laudis\Neo4j\Basic\Driver;
-use Laudis\Neo4j\ClientBuilder;
 use Laudis\Neo4j\Types\ArrayList;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 $database = getenv('NEO4J_DATABASE');
-$database = $database === false ? 'movies' : $database;
+$database = false === $database ? 'movies' : $database;
 
 $uri = getenv('NEO4J_URI');
-$uri = $uri === false ? sprintf('neo4j+s://demo.neo4jlabs.com?database=%s', $database) : $uri;
+$uri = false === $uri ? sprintf('neo4j+s://demo.neo4jlabs.com?database=%s', $database) : $uri;
 
 $user = getenv('NEO4J_USER');
-$user = $user === false ? 'movies' : $user;
+$user = false === $user ? 'movies' : $user;
 
 $password = getenv('NEO4J_PASSWORD');
-$password = $password === false ? 'movies' : $password;
+$password = false === $password ? 'movies' : $password;
 
 $auth = Authenticate::basic($user, $password);
 $driver = Driver::create($uri, authenticate: $auth);
@@ -33,7 +32,8 @@ $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', static function (Request $request, Response $response) {
-    $response->getBody()->write(file_get_contents(__DIR__ . '/public/index.html'));
+    $response->getBody()->write(file_get_contents(__DIR__.'/public/index.html'));
+
     return $response;
 });
 
@@ -47,19 +47,20 @@ RETURN movie.title AS title,
 LIMIT 1
 CYPHER, $args);
 
-    if ($result->count() === 0) {
+    if (0 === $result->count()) {
         $response->getBody()->write(json_encode([
-            'message' => 'Could not find movie with title: "' . $args['title'] . '"'
+            'message' => 'Could not find movie with title: "'.$args['title'].'"',
         ], JSON_THROW_ON_ERROR));
+
         return $response->withStatus(404);
     }
 
     $response->getBody()->write(json_encode($result->first(), JSON_THROW_ON_ERROR));
+
     return $response;
 });
 
 $app->get('/search', static function (Request $request, Response $response) use ($session) {
-
     $result = $session->run(<<<'CYPHER'
 MATCH (movie:Movie) 
 WHERE TOLOWER(movie.title) CONTAINS TOLOWER($title)
@@ -67,6 +68,7 @@ RETURN movie {.title, .tagline, .votes, .released}
 CYPHER, ['title' => $request->getQueryParams()['q'] ?? '']);
 
     $response->getBody()->write(json_encode($result->getResults(), JSON_THROW_ON_ERROR));
+
     return $response;
 });
 
@@ -77,6 +79,7 @@ $app->post('/movie/vote/{title}', static function (Request $request, Response $r
 
     $updates = $result->getSummary()->getCounters()->propertiesSet();
     $response->getBody()->write(json_encode(['updates' => $updates], JSON_THROW_ON_ERROR));
+
     return $response;
 });
 
@@ -92,18 +95,18 @@ CYPHER
 
     foreach ($result as $row) {
         $movieTitle = $row->getAsString('movie');
-        $mappings['Movie:' . $movieTitle] = count($mappings);
+        $mappings['Movie:'.$movieTitle] = count($mappings);
         $tbr['nodes'][] = ['title' => $movieTitle, 'label' => 'movie'];
         /** @var ArrayList<string> $cast */
         $cast = $row->getAsArrayList('cast');
         foreach ($cast as $person) {
-            $number = $mappings['Person:' . $person] ?? null;
-            if ($number === null) {
+            $number = $mappings['Person:'.$person] ?? null;
+            if (null === $number) {
                 $number = count($mappings);
-                $mappings['Person:' . $person] = $number;
+                $mappings['Person:'.$person] = $number;
                 $tbr['nodes'][] = ['title' => $person, 'label' => 'actor'];
             }
-            $tbr['links'][] = ['source' => $number, 'target' => $mappings['Movie:' . $movieTitle]];
+            $tbr['links'][] = ['source' => $number, 'target' => $mappings['Movie:'.$movieTitle]];
         }
     }
 
@@ -113,4 +116,3 @@ CYPHER
 });
 
 $app->run();
-
